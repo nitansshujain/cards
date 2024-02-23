25b2b83880f220b1a62b85f2263c1ee3b78095c4
nitanssj <nitanssj@amdocs.com>
2024-02-23 16:07:45 +0530

    Configuration Management through @Value, Environment Interface, @ConfigurationProperties, Profiles and Arguments & Variables using springBoot

--- src/main/java/com/microservice/cards/CardsApplication.java
+++ src/main/java/com/microservice/cards/CardsApplication.java
@@ -1,12 +1,15 @@
 package com.microservice.cards;
 
+import com.microservice.cards.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaRepositories("com.microservice.cards.repository")
 @EntityScan("com.microservice.cards.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
